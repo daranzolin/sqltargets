ca84fa3f0b70f69a6e7a367ea87a55fb7d6ee5be
David Ranzolin <daranzolin@gmail.com>
2024-04-17 10:39:25 -0700

    test for parameterized queries

--- tests/testthat/test-tar-sql.R
+++ tests/testthat/test-tar-sql.R
@@ -2,7 +2,7 @@ targets::tar_test("tar_sql() works", {
   lines <- c(
     "-- !preview conn=DBI::dbConnect(RSQLite::SQLite())",
     "-- tar_load(data)",
-    "select 1 AS my_col",
+    "select 1 as my_col",
     ""
     )
   writeLines(lines, "query.sql")
@@ -34,4 +34,28 @@ targets::tar_test("tar_sql() works", {
   # Should rerun the query
   suppressMessages(targets::tar_make(callr_function = NULL))
   expect_equal(sort(targets::tar_progress()$name), sort(c("data", "report")))
+})
+
+targets::tar_test("tar_sql() for parameterized queries", {
+  lines <- c(
+    "-- !preview conn=DBI::dbConnect(RSQLite::SQLite())",
+    "-- tar_load(query_params)",
+    "select {val} as {col_name}",
+    ""
+  )
+  writeLines(lines, "query.sql")
+  targets::tar_script({
+    library(sqltargets)
+    list(
+      tar_target(query_params, list(val = 3, col_name = "column1")),
+      tar_sql(
+        report,
+        path = "query.sql",
+        query_params = query_params
+      )
+    )
+  })
+  suppressMessages(targets::tar_make(callr_function = NULL))
+  out <- targets::tar_read(report)
+  expect_equal(out, data.frame(column1 = 3))
 })
