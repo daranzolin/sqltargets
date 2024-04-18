8e9e2b789d35d493a47915c091d098548064e02b
David Ranzolin <daranzolin@gmail.com>
2024-04-18 15:00:16 -0700

    package doc, global var for db con

---
+++ R/sqltargets.R
@@ -0,0 +1,12 @@
+#' \code{sqltargets} package
+#'
+#' targets extension for SQL files
+#'
+#' See the README on
+#' \href{https://github.com/daranzolin/sqltargets#readme}{GitHub}
+#'
+#' @docType package
+#' @name sqltargets
+NULL
+
+utils::globalVariables(c("con"))
