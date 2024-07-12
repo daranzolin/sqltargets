a6ee338d2541906dfd3b138d4406d139f0896d30
daranzolin <daranzolin@gmail.com>
2024-07-12 13:24:54 -0700

    check if params are null

--- R/source_sql_to_dataframe.R
+++ R/source_sql_to_dataframe.R
@@ -1,22 +1,24 @@
-source_sql_to_dataframe <- function(path, params) {
+source_sql_to_dataframe <- function(path, params = NULL) {
 
   connection_string <- stringr::str_extract(readr::read_lines(path, n_max = 1), "(?<=\\=).*")
   connection_call <- paste0("con <- ", connection_string)
   rlang::eval_bare(rlang::parse_expr(connection_call))
   on.exit(DBI::dbDisconnect(con))
   query <- readr::read_file(path)
-  query <- jinjar::render(
-    query,
-    params = params,
-    .config = jinjar::jinjar_config(
-      block_open = sqltargets_option_get("sqltargets.jinja_block_open"),
-      block_close = sqltargets_option_get("sqltargets.jinja_block_close"),
-      variable_open = sqltargets_option_get("sqltargets.jinja_variable_open"),
-      variable_close = sqltargets_option_get("sqltargets.jinja_variable_close"),
-      comment_open = sqltargets_option_get("sqltargets.jinja_comment_open"),
-      comment_close = sqltargets_option_get("sqltargets.jinja_comment_close"),
+  if (!is.null(params)) {
+    query <- jinjar::render(
+      query,
+      params = params,
+      .config = jinjar::jinjar_config(
+        block_open = sqltargets_option_get("sqltargets.jinja_block_open"),
+        block_close = sqltargets_option_get("sqltargets.jinja_block_close"),
+        variable_open = sqltargets_option_get("sqltargets.jinja_variable_open"),
+        variable_close = sqltargets_option_get("sqltargets.jinja_variable_close"),
+        comment_open = sqltargets_option_get("sqltargets.jinja_comment_open"),
+        comment_close = sqltargets_option_get("sqltargets.jinja_comment_close"),
       )
     )
+  }
   out <- DBI::dbGetQuery(con, query)
   msg <- glue::glue("{basename(path)} executed:\n Rows: {nrow(out)}\n Columns: {ncol(out)}")
   cli::cli_alert_success(msg)
