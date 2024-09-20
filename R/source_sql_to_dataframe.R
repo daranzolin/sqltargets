a49ed71ef759a15b01f8c0a21a23653bd00e9689
Jennifer (Jenny) Bryan <jenny.f.bryan@gmail.com>
2024-09-19 18:44:34 -0700

    Prefer glue_data_sql() when data is list-ish, not an actual environment (#22)

--- R/source_sql_to_dataframe.R
+++ R/source_sql_to_dataframe.R
@@ -20,13 +20,13 @@ source_sql_to_dataframe <- function(path, params = NULL) {
       )
     )
   } else {
-    query <- glue::glue_sql(
+    query <- glue::glue_data_sql(
+      params,
       query,
       .con = con,
       .open = sqltargets_option_get("sqltargets.glue_sql_opening_delimiter"),
-      .close = sqltargets_option_get("sqltargets.glue_sql_closing_delimiter"),
-      .envir = params
-      )
+      .close = sqltargets_option_get("sqltargets.glue_sql_closing_delimiter")
+    )
   }
   out <- DBI::dbGetQuery(con, query)
   msg <- glue::glue("{basename(path)} executed:\n Rows: {nrow(out)}\n Columns: {ncol(out)}")
